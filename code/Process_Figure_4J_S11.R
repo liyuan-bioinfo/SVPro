5b64e03677cc886b196d46c02ce1115c42c2bc1d
liyuan-bioinfo <65613804+liyuan-bioinfo@users.noreply.github.com>
2025-07-09 12:29:47 +0800

    Update Process_Figure_4J_S11.R

--- code/Process_Figure_4J_S11.R
+++ code/Process_Figure_4J_S11.R
@@ -7,7 +7,7 @@ library(readxl)
 rm(list = ls())
 
 # set basic info.
-project_dir = "D:\\01_科研项目\\Project3_zhengzhendong\\submit_version"
+project_dir = "SVPro" # modify this to the work dir
 
 input_dir = paste0(project_dir,"\\", "input")
 output_dir = paste0(project_dir,"\\", "output")
